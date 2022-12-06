411027d77bb8f79996008aecbda309722ab023ed
Samir70 <sbetmouni@talktalk.net>
2022-12-06 06:13:08 +0000

    solve day 6

--- lib/day_06.rb
+++ lib/day_06.rb
@@ -2,15 +2,26 @@ class Solution06
   def initialize(file_name)
     @file_name = file_name
     @data = []
+    @buffer = ""
     process
   end
 
-  def run
-  
+  def run(n, len)
+    @buffer = @data[n]
+    i = 0
+    while i <= @buffer.length - len
+      # puts "checking #{@buffer[i-3, 4]}"
+      if unique(@buffer[i, len])
+        return i + len
+      end
+      i += 1
+    end
+    return nil
   end
 
-  def run_2
-  
+  def unique(str)
+    arr = str.split("").uniq
+    return arr.length == str.length
   end
 
   def process
--- spec/day_06_spec.rb
+++ spec/day_06_spec.rb
@@ -6,23 +6,38 @@ RSpec.describe Solution06 do
     @big_test = './lib/inputs/big_tests/day_06.txt'
   end
 
+  it "recognises when four chars are unique" do
+    sol = Solution06.new(@test_case)
+    expect(sol.unique("abcd")).to eq true
+    expect(sol.unique("aacd")).to eq false
+    expect(sol.unique("abbd")).to eq false
+    expect(sol.unique("abca")).to eq false
+  end
   it 'solves example test case (part 1)' do
     sol = Solution06.new(@test_case)
-    expect(sol.run).to eq nil
+    expect(sol.run(0, 4)).to eq 7
+    expect(sol.run(1, 4)).to eq 5
+    expect(sol.run(2, 4)).to eq 6
+    expect(sol.run(3, 4)).to eq 10
+    expect(sol.run(4, 4)).to eq 11
   end
-
+  
   it 'solves example test case (part 2)' do
     sol = Solution06.new(@test_case)
-    expect(sol.run_2).to eq nil
+    expect(sol.run(0, 14)).to eq 19
+    expect(sol.run(1, 14)).to eq 23
+    expect(sol.run(2, 14)).to eq 23
+    expect(sol.run(3, 14)).to eq 29
+    expect(sol.run(4, 14)).to eq 26
   end
-
+  
   it 'solves big test (part 1)' do
     sol = Solution06.new(@big_test)
-    expect(sol.run).to eq nil
+    expect(sol.run(0, 4)).to eq 1953
   end
 
   it 'solves big test (part 2)' do
     sol = Solution06.new(@big_test)
-    expect(sol.run_2).to eq nil
+    expect(sol.run(0, 14)).to eq 2301
   end
 end
