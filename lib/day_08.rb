a5d9b52821ae74771a94e63e9c597dc0e81dc92f
Samir70 <sbetmouni@talktalk.net>
2022-12-08 06:35:43 +0000

    part 1 of day 8

--- lib/day_08.rb
+++ lib/day_08.rb
@@ -6,16 +6,54 @@ class Solution08
   end
 
   def run
-  
+    # puts "#{@data}"
+    vis_trees = []
+    @data.each_with_index do |arr, i|
+      vis_trees = vis_trees.concat(visible(arr, "row", i))
+      vis_trees = vis_trees.concat(visible(arr.reverse, "row-rev", i))
+    end
+    @data.each_index do |j|
+      col = @data.map {|el| el[j]}
+      vis_trees = vis_trees.concat(visible(col, "col", j))
+      vis_trees = vis_trees.concat(visible(col.reverse, "col-rev", j))
+    end
+    return vis_trees.uniq.length
   end
 
   def run_2
-  
+  end
+
+  def make_key(dir, i, j, len)
+    if dir.start_with?("row")
+      if dir == "row"
+        return "#{i}-#{j}"
+      else
+        return "#{i}-#{len - j - 1}"
+      end
+    else
+      if dir == "col"
+        return "#{j}-#{i}"
+      else
+        return "#{len - j - 1}-#{i}"
+      end
+    end
+  end
+
+  def visible(arr, direction, i)
+    count = []
+    max = -1
+    arr.each_with_index do |tree, j|
+      if tree > max
+        max = tree
+        count << make_key(direction, i, j, arr.length)
+      end
+    end
+    return count
   end
 
   def process
     file = File.new(@file_name)
-    @data = file.readlines.map(&:chomp)
+    @data = file.readlines.map(&:chomp).map { |el| el.split("") }.map { |el| el.map(&:to_i) }
     file.close
   end
 end
--- spec/day_08_spec.rb
+++ spec/day_08_spec.rb
@@ -1,27 +1,40 @@
-require 'day_08'
+require "day_08"
 
 RSpec.describe Solution08 do
   before(:each) do
-    @test_case = './lib/inputs/test_cases/day_08_1.txt'
-    @big_test = './lib/inputs/big_tests/day_08.txt'
+    @test_case = "./lib/inputs/test_cases/day_08_1.txt"
+    @big_test = "./lib/inputs/big_tests/day_08.txt"
   end
 
-  it 'solves example test case (part 1)' do
+
+  it "makes keys" do
     sol = Solution08.new(@test_case)
-    expect(sol.run).to eq nil
+    expect(sol.make_key("row", 1, 4, 5)).to eq "1-4"
+    expect(sol.make_key("row-rev", 0, 0, 5)).to eq "0-4"
+    expect(sol.make_key("col", 1, 4, 5)).to eq "4-1"
+    expect(sol.make_key("col-rev", 0, 0, 5)).to eq "4-0"
+  end
+  it "counts visible trees in a single array" do
+    sol = Solution08.new(@test_case)
+    expect(sol.visible([2, 5, 5, 1, 2], "row", 1)).to eq ["1-0", "1-1"]
   end
 
-  it 'solves example test case (part 2)' do
+  it "solves example test case (part 1)" do
+    sol = Solution08.new(@test_case)
+    expect(sol.run).to eq 21
+  end
+
+  it "solves example test case (part 2)" do
     sol = Solution08.new(@test_case)
     expect(sol.run_2).to eq nil
   end
 
-  it 'solves big test (part 1)' do
+  it "solves big test (part 1)" do
     sol = Solution08.new(@big_test)
-    expect(sol.run).to eq nil
+    expect(sol.run).to eq 1672
   end
 
-  it 'solves big test (part 2)' do
+  it "solves big test (part 2)" do
     sol = Solution08.new(@big_test)
     expect(sol.run_2).to eq nil
   end
