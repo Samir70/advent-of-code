e4bbea0c9bd6e31f813fa1fcf7f180514dd0afda
Samir70 <sbetmouni@talktalk.net>
2022-12-09 06:52:51 +0000

    solve day 09

--- lib/day_09.rb
+++ lib/day_09.rb
@@ -3,14 +3,96 @@ class Solution09
     @file_name = file_name
     @data = []
     process
+    @head = [0, 0]
+    @tail = [0, 0]
+    @tail_visited = Set["0, 0"]
+    @knots = []
+    10.times do
+      @knots << [0, 0]
+    end
   end
 
   def run
-  
+    @data.each do |cmd|
+      move(cmd)
+    end
+    return @tail_visited.size
   end
 
   def run_2
-  
+    @data.each do |cmd|
+      move_2(cmd)
+    end
+    return @tail_visited.size
+  end
+
+  def move(str)
+    dir, dist = str.split(" ")
+    dr, dc = { "R" => [0, 1], "L" => [0, -1], "U" => [1, 0], "D" => [-1, 0] }[dir]
+    dist.to_i.times do
+      @head = add_vector(@head, [dr, dc])
+      @tail = update_tail(@head, @tail)
+      @tail_visited.add(make_key(@tail))
+      # puts "#{@head}, #{@tail}"
+    end
+  end
+
+  def move_2(str)
+    dir, dist = str.split(" ")
+    dr, dc = { "R" => [0, 1], "L" => [0, -1], "U" => [1, 0], "D" => [-1, 0] }[dir]
+    dist.to_i.times do
+      @knots[0] = add_vector(@knots[0], [dr, dc])
+      9.times do |i|
+        # puts "Updating knots #{i} and #{i + 1}: #{@knots[i]}, #{@knots[i+1]}"
+        @knots[i + 1] = update_tail(@knots[i], @knots[i + 1])
+      end
+      @tail_visited.add(make_key(@knots[9]))
+      # puts "After #{str}: #{@knots}"
+    end
+  end
+
+  def update_tail(head, tail)
+    hr, hc = head
+    tr, tc = tail
+    if hr == tr
+      if tc == hc || tc == hc + 1 || tc == hc - 1
+        return tail
+      elsif hc - tc > 1
+        return [tr, tc + 1]
+      elsif tc - hc > 1
+        return [tr, tc - 1]
+      end
+    elsif hc == tc
+      if tr == hr || tr == hr + 1 || tr == hr - 1
+        return tail
+      elsif hr - tr > 1
+        return [tr + 1, tc]
+      elsif tr - hr > 1
+        return [tr - 1, tc]
+      end
+    else # Neither the col nor row are the same
+      if Math.hypot(hr - tr, hc - tc) < 2
+        return tail
+      elsif hr > tr && hc > tc # above right
+        return [tr + 1, tc + 1]
+      elsif hr > tr && hc < tc # above left
+        return [tr + 1, tc - 1]
+      elsif hr < tr && hc > tc # below right
+        return [tr - 1, tc + 1]
+      elsif hr < tr && hc < tc # below left
+        return [tr - 1, tc - 1]
+      end
+    end
+    return tail
+  end
+
+  def add_vector(a, b)
+    # puts "adding #{a} and #{b}"
+    return [a[0] + b[0], a[1] + b[1]]
+  end
+
+  def make_key(arr)
+    return arr.join(", ")
   end
 
   def process
@@ -18,4 +100,10 @@ class Solution09
     @data = file.readlines.map(&:chomp)
     file.close
   end
+
+  def tail_visited
+    return @tail_visited.size
+  end
+
+  attr_reader :head, :tail
 end
--- spec/day_09_spec.rb
+++ spec/day_09_spec.rb
@@ -1,27 +1,77 @@
-require 'day_09'
+require "day_09"
 
 RSpec.describe Solution09 do
   before(:each) do
-    @test_case = './lib/inputs/test_cases/day_09_1.txt'
-    @big_test = './lib/inputs/big_tests/day_09.txt'
+    @test_case = "./lib/inputs/test_cases/day_09_1.txt"
+    @test_case_2 = "./lib/inputs/test_cases/day_09_2.txt"
+    @big_test = "./lib/inputs/big_tests/day_09.txt"
+  end
+  context "One direction, 4 steps" do
+    it "moves the head R 4" do
+      sol = Solution09.new(@test_case)
+      sol.move("R 4")
+      expect(sol.head).to eq [0, 4]
+      expect(sol.tail).to eq [0, 3]
+    end
+    it "moves the head L 4" do
+      sol = Solution09.new(@test_case)
+      sol.move("L 4")
+      expect(sol.head).to eq [0, -4]
+      expect(sol.tail).to eq [0, -3]
+    end
+    it "moves the head U 4" do
+      sol = Solution09.new(@test_case)
+      sol.move("U 4")
+      expect(sol.head).to eq [4, 0]
+      expect(sol.tail).to eq [3, 0]
+    end
+    it "moves the head D 4" do
+      sol = Solution09.new(@test_case)
+      sol.move("D 4")
+      expect(sol.head).to eq [-4, 0]
+      expect(sol.tail).to eq [-3, 0]
+    end
+  end
+  context "multi command moves" do
+    it "moves the head R 4; U 4" do
+      sol = Solution09.new(@test_case)
+      sol.move("R 4")
+      sol.move("U 4")
+      expect(sol.head).to eq [4, 4]
+      expect(sol.tail).to eq [3, 4]
+      expect(sol.tail_visited).to eq 7
+    end
+    it "moves the head R 4; U 4; L 3" do
+      sol = Solution09.new(@test_case)
+      sol.move("R 4")
+      sol.move("U 4")
+      sol.move("L 3")
+      expect(sol.head).to eq [4, 1]
+      expect(sol.tail).to eq [4, 2]
+      expect(sol.tail_visited).to eq 9
+    end
   end
 
-  it 'solves example test case (part 1)' do
+  it "solves example test case (part 1)" do
     sol = Solution09.new(@test_case)
-    expect(sol.run).to eq nil
+    expect(sol.run).to eq 13
   end
 
-  it 'solves example test case (part 2)' do
+  it "solves example test case (part 2)" do
     sol = Solution09.new(@test_case)
-    expect(sol.run_2).to eq nil
+    expect(sol.run_2).to eq 1
+  end
+  it "solves example test case (part 2b)" do
+    sol = Solution09.new(@test_case_2)
+    expect(sol.run_2).to eq 36
   end
 
-  it 'solves big test (part 1)' do
+  it "solves big test (part 1)" do
     sol = Solution09.new(@big_test)
-    expect(sol.run).to eq nil
+    expect(sol.run).to eq 6354
   end
 
-  it 'solves big test (part 2)' do
+  it "solves big test (part 2)" do
     sol = Solution09.new(@big_test)
     expect(sol.run_2).to eq nil
   end
