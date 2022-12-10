db41ff8cfed0839121234ee255b616e72992494a
Samir70 <sbetmouni@talktalk.net>
2022-12-10 06:41:51 +0000

    solve say 10, not all TDD

--- lib/day_10.rb
+++ lib/day_10.rb
@@ -2,15 +2,80 @@ class Solution10
   def initialize(file_name)
     @file_name = file_name
     @data = []
+    @x = [1] # after 0th cycle x contains 1
     process
   end
 
+  attr_reader :x
+
   def run
-  
+    @data.each do |cmd|
+      cpu(cmd)
+      # puts "#{cmd}, x[#{x.length}-1] #{@x[-1]}"
+    end
+    # puts "finally #{@x.length}"
+    return nil
   end
 
   def run_2
-  
+    crt = ""
+    240.times do |i|
+      if print?(i+1)
+        crt += "#"
+      else
+        crt += "."
+      end
+    end
+    puts "", crt
+    return nil
+  end
+
+  def noop
+    @x << @x.last
+  end
+
+  def addx(instr)
+    amount = instr.split(" ")[1].to_i
+    noop
+    @x << @x.last + amount
+  end
+
+  def cpu(instr)
+    if instr == "noop"
+      noop
+    elsif instr.start_with?("addx")
+      addx(instr)
+    end
+  end
+
+  def sig_strength(n)
+    val = @x[n-1]
+    # puts "x is #{val}, #{@x}"
+    return val.nil? ? nil : val * n
+  end
+
+  def sigs(arr)
+    return arr.map { |el| sig_strength(el) }
+  end
+
+  def print?(n)
+    if n == 10
+      puts "#{10}:::: #{@x[9]}"
+    end
+    val = @x[n-1]
+    if val.nil?
+      puts "got nil print?(#{n})"
+      return false
+    end
+    n = n % 40
+    if n == 0
+      n = 40
+    end
+    n -= 1
+    if n == val - 1 || n == val || n == val + 1
+      return true
+    end
+    return false
   end
 
   def process
--- spec/day_10_spec.rb
+++ spec/day_10_spec.rb
@@ -6,23 +6,68 @@ RSpec.describe Solution10 do
     @big_test = './lib/inputs/big_tests/day_10.txt'
   end
 
-  it 'solves example test case (part 1)' do
+  it "can follow a noop instruction" do
     sol = Solution10.new(@test_case)
-    expect(sol.run).to eq nil
+    sol.cpu("noop")
+    expect(sol.x).to eq [1, 1]
   end
 
-  it 'solves example test case (part 2)' do
+  it "can follow an addx instruction" do
+    sol = Solution10.new(@test_case)
+    sol.cpu("addx 3")
+    expect(sol.x).to eq [1, 1, 4]
+  end
+
+  it "can find the signal strength at given time" do
+    sol = Solution10.new(@test_case)
+    sol.run
+    expect(sol.sig_strength(20)).to eq 21*20
+    expect(sol.sig_strength(60)).to eq 19*60
+  end
+
+  it "can find the strengths at many times" do
+    sol = Solution10.new(@test_case)
+    sol.run
+    expect(sol.sigs([20, 60, 100, 140, 180, 220])).to eq [420, 1140, 1800, 2940, 2880, 3960]
+  end
+
+  it "detects if sprite is at print point" do
+    sol = Solution10.new(@test_case)
+    sol.run
+    expect(sol.print?(1)).to eq true
+    expect(sol.print?(2)).to eq true
+    expect(sol.print?(3)).to eq false
+    expect(sol.print?(4)).to eq false
+    expect(sol.print?(5)).to eq true
+    expect(sol.print?(10)).to eq true
+  end
+
+  it "finds finds all print points" do
+    sol = Solution10.new(@test_case)
+    sol.run
+    sol.run_2
+  end
+
+  it 'solves example test case (part 1)' do
+    sol = Solution10.new(@test_case)
+    sol.run
+    expect(sol.sigs([20, 60, 100, 140, 180, 220]).sum).to eq 13140
+  end
+
+  xit 'solves example test case (part 2)' do
     sol = Solution10.new(@test_case)
     expect(sol.run_2).to eq nil
   end
 
   it 'solves big test (part 1)' do
     sol = Solution10.new(@big_test)
-    expect(sol.run).to eq nil
+    sol.run
+    expect(sol.sigs([20, 60, 100, 140, 180, 220]).sum).to eq 13740
   end
 
   it 'solves big test (part 2)' do
     sol = Solution10.new(@big_test)
-    expect(sol.run_2).to eq nil
+    sol.run
+    sol.run_2 #gives ZUPRFECL
   end
 end
