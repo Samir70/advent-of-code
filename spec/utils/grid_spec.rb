0a9afaee1726de14840178317515525bd8631c13
Samir70 <sbetmouni@talktalk.net>
2022-12-10 12:43:15 +0000

    add a grid class for making a viewing

---
+++ spec/utils/grid_spec.rb
@@ -0,0 +1,46 @@
+require_relative "../../utils/grid"
+
+RSpec.describe Grid do
+  before(:each) do
+    points = [
+      [0, 2], [5, 7], [-3, 2], [4, -2],
+    ]
+    @grid = Grid.new(points, ".", "index")
+  end
+  context "finding size of grid" do
+    it "finds the min row value of the points" do
+      expect(@grid.min_row).to eq -3
+    end
+    it "finds the max row value of the points" do
+      expect(@grid.max_row).to eq 5
+    end
+    it "finds the min column value of the points" do
+      expect(@grid.min_col).to eq -2
+    end
+    it "finds the max column value of the points" do
+      expect(@grid.max_col).to eq 7
+    end
+    it "finds the width of the grid" do
+      expect(@grid.width).to eq 10
+    end
+    it "finds the height of the grid" do
+      expect(@grid.height).to eq 9
+    end
+  end
+  it "shifts all points to non-neg" do
+    expect(@grid.points).to eq [[3, 4], [8, 9], [0, 4], [7, 0]]
+  end
+  it "makes a grid" do
+    expect(@grid.grid).to eq [
+         [".", ".", ".", ".", 2, ".", ".", ".", ".", "."],
+         [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+         [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+         [".", ".", ".", ".", 0, ".", ".", ".", ".", "."],
+         [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+         [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+         [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+         [3, ".", ".", ".", ".", ".", ".", ".", ".", "."],
+         [".", ".", ".", ".", ".", ".", ".", ".", ".", 1],
+       ]
+  end
+end
---
+++ utils/grid.rb
@@ -0,0 +1,39 @@
+class Grid
+  def initialize(points, blank_char, fill_char)
+    @blank_char = blank_char
+    @fill_char = fill_char
+    @min_row = points.map { |p| p[0] }.min
+    @min_col = points.map { |p| p[1] }.min
+    @max_row = points.map { |p| p[0] }.max
+    @max_col = points.map { |p| p[1] }.max
+    @width = 1 + @max_col - @min_col
+    @height = 1 + @max_row - @min_row
+    @points = points.map { |p| [p[0] - @min_row, p[1] - @min_col] }
+    @grid = []
+    make_grid
+    put_points_on_grid
+  end
+
+  def to_s
+    @grid.each do |row|
+      puts "#{row}"
+    end
+  end
+
+  def make_grid
+    @height.times do
+      row = Array.new(@width, @blank_char)
+      @grid << row
+    end
+  end
+
+  def put_points_on_grid
+    @points.each_with_index do |p, i|
+      r, c = p
+      # puts "point #{p}, with index #{i} give #{r}, #{c}"
+      @grid[r][c] = (@fill_char == "index" ? i : @fill_char)
+    end
+  end
+
+  attr_reader :min_row, :min_col, :max_col, :max_row, :width, :height, :points, :grid
+end
