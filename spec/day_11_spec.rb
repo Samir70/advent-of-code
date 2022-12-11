e12896ff5b021c56296b1023360828d57dda42fb
Samir70 <sbetmouni@talktalk.net>
2022-12-11 05:22:34 +0000

    add a test for day 11

--- spec/day_11_spec.rb
+++ spec/day_11_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe Solution11 do
     @big_test = './lib/inputs/big_tests/day_11.txt'
   end
 
+  it "groups input by monkey" do
+    sol = Solution11.new(@test_case)
+    expect(sol.split_by_element([1, 2, 0, 4, 6, 8, 0, 9], 0)).to eq [[1, 2], [4, 6, 8], [9]]
+  end
+
   it 'solves example test case (part 1)' do
     sol = Solution11.new(@test_case)
     expect(sol.run).to eq nil
