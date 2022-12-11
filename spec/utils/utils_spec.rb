796b4eb500f6e036d599fd4dd746ada7eac23642
Samir70 <sbetmouni@talktalk.net>
2022-12-11 10:47:57 +0000

    add do-op to utils

--- spec/utils/utils_spec.rb
+++ spec/utils/utils_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe "Utils" do
       expect(string_to_grid("12345123451234512345", 5)).to eq ["12345", "12345", "12345", "12345"]
     end
   end
+
   context "#split_by_elements" do
     it "splits a list of numbers" do
       expect(split_by_element([1, 2, 0, 4, 6, 8, 0, 9], 0)).to eq [[1, 2], [4, 6, 8], [9]]
@@ -20,6 +21,25 @@ RSpec.describe "Utils" do
     end
     it "splits a list of lists" do
       expect(split_by_element([[1, 2, 0], [101], [4, 6, 8], [0, 9]], [101])).to eq [[[1, 2, 0]], [[4, 6, 8], [0, 9]]]
+    end
+  end
+
+  context "#do-op" do
+    it "performs number */+- number operations given" do
+      expect(do_op(5, "*", 6)).to eq 30
+      expect(do_op(5, "*", 6.0)).to eq 30.0
+      expect(do_op(42, "/", 6)).to eq 7
+      expect(do_op(5, "+", 6)).to eq 11
+      expect(do_op(5, "-", 6)).to eq -1
+    end
+    it "performs operation given entirely as string" do
+      expect(do_op("5 * 6")).to eq 30
+      expect(do_op("42/ -6")).to eq -7
+      expect(do_op("5 +6")).to eq 11
+      expect(do_op("5 - 6")).to eq -1
+      expect(do_op("5 - -6")).to eq 11
+      expect(do_op("5- -6")).to eq 11
+      expect(do_op("5 * self")).to eq 25
     end
   end
 end
--- utils/utils.rb
+++ utils/utils.rb
@@ -2,7 +2,7 @@ def string_to_grid(str, width)
   out = []
   first = 0
   last = width
-  while first < str.length do
+  while first < str.length
     out << str[first...last]
     first = last
     last += width
@@ -25,3 +25,24 @@ def split_by_element(arr, val)
   return out
 end
 
+def do_op(*args)
+  if args.length == 3
+    a, op, b = args
+  else
+    a, op, b = args[0].split(/(\*|\/|\+|\-\s)/)
+    op = "-" if op == "- "
+    a = a.to_i
+    b = (b == "self" || b == " self") ? a : b.to_i
+    return do_op(a.to_i, op, b)
+  end
+  if op == "*"
+    return a * b # day_11 used (b == 0 ? a : b)
+  elsif op == "/"
+    return a / b
+  elsif op == "+"
+    return a + b
+  elsif op == "-"
+    return a - b
+  end
+  return nil
+end
