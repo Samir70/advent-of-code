6405f21ee7af90eb96c5353bfe9a98c9dcc9b0e2
Samir70 <sbetmouni@talktalk.net>
2022-12-11 05:30:21 +0000

    add test to day 11

--- spec/day_11_spec.rb
+++ spec/day_11_spec.rb
@@ -1,9 +1,9 @@
-require 'day_11'
+require "day_11"
 
 RSpec.describe Solution11 do
   before(:each) do
-    @test_case = './lib/inputs/test_cases/day_11_1.txt'
-    @big_test = './lib/inputs/big_tests/day_11.txt'
+    @test_case = "./lib/inputs/test_cases/day_11_1.txt"
+    @big_test = "./lib/inputs/big_tests/day_11.txt"
   end
 
   it "groups input by monkey" do
@@ -11,22 +11,32 @@ RSpec.describe Solution11 do
     expect(sol.split_by_element([1, 2, 0, 4, 6, 8, 0, 9], 0)).to eq [[1, 2], [4, 6, 8], [9]]
   end
 
-  it 'solves example test case (part 1)' do
+  it "extracts Monkey data" do
+    sol = Solution11.new(@test_case)
+    expect(sol.monkeys.length).to 4
+    expect(sol.monkeys.first.items).to eq [79, 98]
+    expect(sol.monkeys.first.operation).to eq ["*", 19]
+    expect(sol.monkeys.first.div_test).to eq 23
+    expect(sol.monkeys.first.true_monkey).to eq 2
+    expect(sol.monkeys.first.false_monkey).to eq 3
+  end
+
+  it "solves example test case (part 1)" do
     sol = Solution11.new(@test_case)
     expect(sol.run).to eq nil
   end
 
-  it 'solves example test case (part 2)' do
+  it "solves example test case (part 2)" do
     sol = Solution11.new(@test_case)
     expect(sol.run_2).to eq nil
   end
 
-  it 'solves big test (part 1)' do
+  it "solves big test (part 1)" do
     sol = Solution11.new(@big_test)
     expect(sol.run).to eq nil
   end
 
-  it 'solves big test (part 2)' do
+  it "solves big test (part 2)" do
     sol = Solution11.new(@big_test)
     expect(sol.run_2).to eq nil
   end
