073f170791c6ac7ef5c290a5216445a0aeae918e
Samir70 <sbetmouni@talktalk.net>
2022-12-13 09:14:30 +0000

    solve day 13

--- lib/day_13.rb
+++ lib/day_13.rb
@@ -1,21 +1,142 @@
+require_relative "../utils/utils"
+require "JSON"
+
 class Solution13
   def initialize(file_name)
     @file_name = file_name
     @data = []
+    @data2 = []
     process
   end
 
+  attr_reader :data
+
   def run
-  
+    comps = compare_all
+    sum = 0
+    comps.each_with_index do |val, i|
+      sum += i + 1 if val
+    end
+    return sum
   end
 
   def run_2
-  
+    @data2.sort! {|a, b| compare(a, b) ? -1 : 1}
+    # @data2.each do |line|
+    #   puts "#{line}"
+    # end
+
+    a = @data2.index([[2]]) + 1
+    b = @data2.index([[6]]) + 1
+    return a * b
+  end
+
+  def compare(a, b)
+    i = 0
+    j = 0
+    # puts "comparing #{a}"
+    # puts "and #{b}"
+    outcome = "same so far"
+    while i < a.length && j < b.length
+      if a[i].class == Array && b[j].class == Array
+        # puts "found two arrays #{a[i]} and #{b[j]}"
+        outcome = compare(a[i], b[j])
+        # puts "got #{outcome}"
+        if outcome == false
+          return false
+        end
+      elsif a[i].class == Array && b[j].class == Integer
+        # puts "found arr, int"
+        outcome = compare(a[i], [b[j]])
+        # puts "got #{outcome}"
+        if outcome == false
+          return false
+        end
+      elsif a[i].class == Integer && b[j].class == Array
+        # puts "found int, arr"
+        outcome = compare([a[i]], b[j])
+        # puts "got #{outcome}"
+        if outcome == false
+          return false
+        end
+      else
+        # puts "comparing #{a[i]} and #{b[j]}"
+        outcome = a[i] == b[j] ? "same so far" : a[i] < b[j]
+      end
+      return outcome if outcome != "same so far"
+      i += 1
+      j += 1
+    end
+    # return "same so far" if a.length == b.length
+    return a.length == b.length ? "same so far" : a.length < b.length
+  end
+
+  def compare_old(a, b)
+    i = 0
+    j = 0
+    puts "comparing #{a}"
+    puts "and #{b}"
+    while i < a.length && j < b.length
+      if a[i].class == Array && b[j].class == Array
+        puts "found two arrays"
+        outcome = compare(a[i], b[j])
+        puts "got #{outcome}"
+        if outcome == false
+          return false
+        end
+      elsif a[i].class == Array && b[j].class == Integer
+        puts "found arr, int"
+        outcome = compare(a[i], [b[j]])
+        puts "got #{outcome}"
+        if outcome == false
+          return false
+        end
+      elsif a[i].class == Integer && b[j].class == Array
+        puts "found int, arr"
+        outcome = compare([a[i]], b[j])
+        puts "got #{outcome}"
+        if outcome == false
+          return false
+        end
+      else
+        puts "comparing #{a[i]} and #{b[j]}"
+        return true if a[i] < b[j]
+        return false if a[i] > b[j]
+      end
+      i += 1
+      j += 1
+    end
+    return true if a.length <= b.length
+    return false if a.length > b.length
+  end
+
+  def compare_all
+    out = []
+    @data.each do |pair|
+      left, right = pair
+      result = compare(left, right)
+      # puts "#{left}"
+      # puts "#{right}"
+      # puts result
+      # puts ""
+      out << result
+    end
+    return out
   end
 
   def process
     file = File.new(@file_name)
-    @data = file.readlines.map(&:chomp)
+    @data = split_by_element(
+      file.readlines.map(&:chomp).map { |line| line == "" ? "" : JSON.parse(line) },
+      ""
+    )
     file.close
+
+    file = File.new(@file_name)
+    @data2 = file.readlines
+      .map(&:chomp).map { |line| line == "" ? "" : JSON.parse(line) }
+      .select {|line| line != ""}
+    file.close
+    @data2.concat([[[2]], [[6]]])
   end
 end
--- spec/day_13_spec.rb
+++ spec/day_13_spec.rb
@@ -1,28 +1,59 @@
-require 'day_13'
+require "day_13"
 
 RSpec.describe Solution13 do
   before(:each) do
-    @test_case = './lib/inputs/test_cases/day_13_1.txt'
-    @big_test = './lib/inputs/big_tests/day_13.txt'
+    @test_case = "./lib/inputs/test_cases/day_13_1.txt"
+    @big_test = "./lib/inputs/big_tests/day_13.txt"
   end
 
-  it 'solves example test case (part 1)' do
+  # it "splits data into pairs" do
+  #   sol = Solution13.new(@test_case)
+  #   expect(sol.data[0]).to eq [[1, 1, 3, 1, 1], [1, 1, 5, 1, 1]]
+  # end
+
+  it "compares arr of int" do
     sol = Solution13.new(@test_case)
-    expect(sol.run).to eq nil
+    expect(sol.compare([1, 1, 3, 1, 1], [1, 1, 5, 1, 1])).to eq true
+    expect(sol.compare([1, 4, 3, 1, 1], [1, 1, 5, 1, 1])).to eq false
+    expect(sol.compare([1, 1, 3, 1, 1], [1, 1, 3, 1])).to eq false
+    expect(sol.compare([1, 1, 3, 1], [1, 1, 3, 1, 1])).to eq true
   end
 
-  it 'solves example test case (part 2)' do
+  it "Compare [[1],[2,3,4]] vs [[1],4]" do
     sol = Solution13.new(@test_case)
-    expect(sol.run_2).to eq nil
+    expect(sol.compare([[1], [2, 3, 4]], [[1], 4])).to eq true
+    expect(sol.compare([[1], 4], [[1], [2, 3, 4]])).to eq false
+  end
+  # it "Compares all pairs" do
+  #   sol = Solution13.new(@test_case)
+  #   expect(sol.compare_all).to eq [true, true, false, true, false, true, false, false]
+  # end
+
+  it "compares [[4, [4], [[10, 7, 2], [1, 6, 5, 7, 4], [7, 3, 3, 1, 5], []], 1], [[], [[7, 6, 3]], 5, 5]]
+  [[[10]]]" do
+    sol = Solution13.new(@test_case)
+    expect(sol.compare([[4, [4], [[10, 7, 2], [1, 6, 5, 7, 4], [7, 3, 3, 1, 5], []], 1], [[], [[7, 6, 3]], 5, 5]],
+    [[[10]]])).to eq true
   end
 
-  it 'solves big test (part 1)' do
-    sol = Solution13.new(@big_test)
-    expect(sol.run).to eq nil
+  it "solves example test case (part 1)" do
+    sol = Solution13.new(@test_case)
+    expect(sol.run).to eq 13
   end
 
-  it 'solves big test (part 2)' do
+  it "solves example test case (part 2)" do
+    sol = Solution13.new(@test_case)
+    expect(sol.run_2).to eq 140
+  end
+
+  it "solves big test (part 1)" do
     sol = Solution13.new(@big_test)
-    expect(sol.run_2).to eq nil
+    expect(sol.run).to eq 6395
+    # 466 is too low
+  end
+
+  it "solves big test (part 2)" do
+    sol = Solution13.new(@big_test)
+    expect(sol.run_2).to eq 24921
   end
 end
