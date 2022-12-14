b24ea6dcf26ac0f00ffb50796840691b3e2c2ec2
Samir70 <sbetmouni@talktalk.net>
2022-12-14 10:25:03 +0000

    solve day 14

--- lib/day_14.rb
+++ lib/day_14.rb
@@ -1,16 +1,145 @@
+require_relative "../utils/grid_from_points"
+
 class Solution14
   def initialize(file_name)
     @file_name = file_name
     @data = []
+    @grid = []
+    @entry = [0, 500]
     process
   end
 
+  attr_reader :grid, :entry
+
   def run
-  
+    make_grid
+    result = drop_sand
+    count = 0
+    while result != nil
+      count += 1
+      result = drop_sand
+    end
+    return count
   end
 
   def run_2
-  
+    make_grid2
+    @grid.add_row(".")
+    @grid.add_row("#")
+    result = drop_sand
+    count = 0
+    while result != "full" && count < 100000
+      count += 1
+      result = drop_sand
+    end
+    return count + 1
+  end
+
+  def extract_points(str)
+    points = []
+    xy = str.split("->")
+    xy.each do |p|
+      c, r = p.split(",").map(&:to_i)
+      points << [r, c]
+    end
+    return points
+  end
+
+  def make_lines(str)
+    points = Set.new()
+    corners = extract_points(str)
+    prev = corners[0]
+    points << prev
+    corner_idx = 1
+    while corner_idx < corners.length
+      cur = corners[corner_idx]
+      r, c = prev
+      i, j = cur
+      prev = [i, j]
+      if r == i
+        j, c = [c, j] if j < c
+        for x in c..j
+          points.add([r, x])
+        end
+      elsif c == j
+        i, r = [r, i] if i < r
+        for y in r..i
+          points.add([y, c])
+        end
+      else
+        return "neither row nor column match #{[r, c]} -> #{[i, j]}"
+      end
+      corner_idx += 1
+    end
+    return points
+  end
+
+  def make_grid
+    points = [[0, 500]]
+    @data.each do |line|
+      new_points = make_lines(line)
+      if new_points.class == Set
+        points.concat(Array(new_points))
+      else
+        puts new_points
+      end
+    end
+    @grid = GridFromPoints.new(points, ".", "#")
+    @entry = [0, 500 - @grid.min_col]
+    @grid.change_point(@entry[0], @entry[1], "+")
+  end
+  def make_grid2
+    points = [[0, 335], [0, 665], [0, 500]]
+    @data.each do |line|
+      new_points = make_lines(line)
+      if new_points.class == Set
+        points.concat(Array(new_points))
+      else
+        puts new_points
+      end
+    end
+    @grid = GridFromPoints.new(points, ".", "#")
+    @entry = [0, 500 - @grid.min_col]
+    @grid.change_point(@entry[0], @entry[1], "+")
+  end
+
+  def drop_sand
+    r, c = @entry
+    b_left, b, b_right = look_below(r, c)
+    return "full" if [b_left, b, b_right] == ["o", "o", "o"]
+    while !settled?(b_left, b, b_right)
+      # puts "below #{[r, c]}: #{b_left}, #{b}, #{b_right}    settled: #{settled?(b_left, b, b_right)}"
+      if b == "."
+        r += 1
+      elsif b_left == "."
+        r += 1
+        c -= 1
+      else
+        r += 1
+        c += 1
+      end
+      b_left, b, b_right = look_below(r, c)
+    end
+    if [b_left, b, b_right].include?(nil)
+      return nil
+    end
+    @grid.change_point(r, c, "o")
+    # puts grid
+    return [r, c]
+  end
+
+  def settled?(b_left, b, b_right)
+    if b == "." || b_left == "." || b_right == "."
+      return false
+    end
+    return true
+  end
+
+  def look_below(r, c)
+    b_left = r + 1 < @grid.height ? @grid.grid[r + 1][c - 1] : nil
+    b_right = r + 1 < @grid.height ? @grid.grid[r + 1][c + 1] : nil
+    b = r + 1 < @grid.height ? @grid.grid[r + 1][c] : nil
+    return [b_left, b, b_right]
   end
 
   def process
--- spec/day_14_spec.rb
+++ spec/day_14_spec.rb
@@ -1,28 +1,100 @@
-require 'day_14'
+require "day_14"
 
 RSpec.describe Solution14 do
   before(:each) do
-    @test_case = './lib/inputs/test_cases/day_14_1.txt'
-    @big_test = './lib/inputs/big_tests/day_14.txt'
+    @test_case = "./lib/inputs/test_cases/day_14_1.txt"
+    @big_test = "./lib/inputs/big_tests/day_14.txt"
   end
 
-  it 'solves example test case (part 1)' do
+  it "extracts points from 10,3 -> 10,6 -> 8,6" do
     sol = Solution14.new(@test_case)
-    expect(sol.run).to eq nil
+    expect(sol.extract_points("10,3 -> 10,6 -> 8,6")).to eq [[3, 10], [6, 10], [6, 8]]
   end
-
-  it 'solves example test case (part 2)' do
+  it "makes lines from 10,3 -> 10,6" do
     sol = Solution14.new(@test_case)
-    expect(sol.run_2).to eq nil
+    expect(sol.make_lines("10,3 -> 10,6")).to eq Set.new([[3, 10], [4, 10], [5, 10], [6, 10]])
+  end
+  it "makes lines from 10,6 -> 10,3" do
+    sol = Solution14.new(@test_case)
+    expect(sol.make_lines("10,6 -> 10,3")).to eq Set.new([[3, 10], [4, 10], [5, 10], [6, 10]])
+  end
+  it "makes lines from 10,6 -> 13,6" do
+    sol = Solution14.new(@test_case)
+    expect(sol.make_lines("10,6 -> 13,6")).to eq Set.new([[6, 10], [6, 11], [6, 12], [6, 13]])
+  end
+  it "makes lines from 13,6 -> 10,6" do
+    sol = Solution14.new(@test_case)
+    expect(sol.make_lines("13,6 -> 10,6")).to eq Set.new([[6, 10], [6, 11], [6, 12], [6, 13]])
+  end
+  it "reports diagonal when trying to make lines from 10,3 -> 13,6" do
+    sol = Solution14.new(@test_case)
+    expect(sol.make_lines("10,3 -> 13,6")).to eq "neither row nor column match [3, 10] -> [6, 13]"
+  end
+  it "makes a grid" do
+    sol = Solution14.new(@test_case)
+    sol.make_grid
+    expect(sol.grid.min_row).to eq 0
+    expect(sol.grid.min_col).to eq 494
+    expect(sol.entry).to eq [0, 6]
+    expect(sol.grid.grid).to eq [
+         "......+...".split(""),
+         "..........".split(""),
+         "..........".split(""),
+         "..........".split(""),
+         "....#...##".split(""),
+         "....#...#.".split(""),
+         "..###...#.".split(""),
+         "........#.".split(""),
+         "........#.".split(""),
+         "#########.".split("")
+       ]
   end
 
-  it 'solves big test (part 1)' do
-    sol = Solution14.new(@big_test)
-    expect(sol.run).to eq nil
+  it "detects when sand is settled" do
+    sol = Solution14.new(@test_case)
+    expect(sol.settled?("#", "#", "#")).to eq true
+    expect(sol.settled?(".", "#", "#")).to eq false
+    expect(sol.settled?("#", ".", "#")).to eq false
+    expect(sol.settled?("#", "#", ".")).to eq false
+    expect(sol.settled?(nil, "#", ".")).to eq false
   end
 
-  it 'solves big test (part 2)' do
+  it "drops sand" do
+    sol = Solution14.new(@test_case)
+    sol.make_grid
+    expect(sol.drop_sand).to eq [8, 6]
+    expect(sol.grid.grid[8][6]).to eq "o"
+  end
+  it "drops many grains of sand" do
+    sol = Solution14.new(@test_case)
+    sol.make_grid
+    expect(sol.drop_sand).to eq [8, 6]
+    expect(sol.grid.grid[8][6]).to eq "o"
+    expect(sol.drop_sand).to eq [8, 5]
+    expect(sol.grid.grid[8][5]).to eq "o"
+    expect(sol.drop_sand).to eq [8, 7]
+    expect(sol.grid.grid[8][7]).to eq "o"
+    expect(sol.drop_sand).to eq [7, 6]
+    expect(sol.grid.grid[7][6]).to eq "o"
+  end
+
+  it "solves example test case (part 1)" do
+    sol = Solution14.new(@test_case)
+    expect(sol.run).to eq 24
+  end
+
+  it "solves example test case (part 2)" do
+    sol = Solution14.new(@test_case)
+    expect(sol.run_2).to eq 93
+  end
+
+  it "solves big test (part 1)" do
     sol = Solution14.new(@big_test)
-    expect(sol.run_2).to eq nil
+    expect(sol.run).to eq 1199
+  end
+
+  it "solves big test (part 2)" do
+    sol = Solution14.new(@big_test)
+    expect(sol.run_2).to eq 23925
   end
 end
