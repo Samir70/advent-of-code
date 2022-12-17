a0d83e33cb518dbb90b909c793d9db36fb1a6e3c
Samir70 <sbetmouni@talktalk.net>
2022-12-17 09:49:06 +0000

    solve days 16 part 1

--- lib/day_16.rb
+++ lib/day_16.rb
@@ -2,20 +2,127 @@ class Solution16
   def initialize(file_name)
     @file_name = file_name
     @data = []
+    @graph = {}
+    @pList = PointList.new()
     process
   end
 
+  attr_reader :graph, :pList
+
   def run
-  
+    make_edges
+    new_stack = []
+    counter = 0
+    max_gas = 0
+    @graph["AA"][:gas_when_visited_at_t][30] = 0
+    move_stack = next_moves("AA", []).map {|e| Move.new(30, 0, [], e)}
+    while move_stack.length > 0 || new_stack.length > 0 do
+      visits_to_update = []
+      while move_stack.length > 0 do
+        cur = move_stack.pop()
+        time, gas, opened, cur_loc = cur.execute
+        visits_to_update << [cur_loc, gas, time]
+        if time >= 0
+          max_gas = gas if gas > max_gas
+        end
+        follow_this = false #opened.join(",").start_with?("DD,BB,JJ")
+        puts "#{cur} #{[time, gas, opened, cur_loc]}" if follow_this
+        edges_from_here = next_moves(cur_loc, opened)
+        # puts "#{edges_from_here}" if opened.join(",").start_with?("DD")
+        edges_from_here.each do |edge|
+          good_move = true
+          good_move = false if time - edge.cost < 0
+          good_move = false if @graph[edge.destination][:gas_when_visited_at_t][time] > gas
+          puts "#{edge}, good?#{good_move}" if follow_this
+          new_stack << Move.new(time, gas, [].concat(opened), edge) if good_move
+        end
+      end
+      # puts "counter: #{counter}, gas: #{max_gas}, new_stack has #{new_stack.length} moves"
+      # puts "visits to update #{visits_to_update}"
+      visits_to_update.each do |loc, gas, time|
+        @graph[loc][:gas_when_visited_at_t][time] = gas if gas > @graph[loc][:gas_when_visited_at_t][time]
+      end
+      move_stack.concat(new_stack) if counter < 30
+      counter += 1
+      new_stack = []
+    end
+    return max_gas
   end
 
   def run_2
-  
+  end
+
+  def parse(str)
+    words = str.split(" ")
+    cur = words[1]
+    rate = words[4].split("=")[1].to_i
+    valves = words[9..-1].join("").split(",")
+    @graph[cur] = { rate: rate, valves: valves, gas_when_visited_at_t: Array.new(31, -1) }
+  end
+
+  def make_edges
+    @data.each do |line|
+      parse(line)
+    end
+
+    @graph.each do |key, val|
+      # puts "#{key} => #{val}"
+      val[:valves].each do |dest|
+        @pList.add_edge(key, Edge.new(key, dest, 1, 0))
+        rate = @graph[dest][:rate]
+        @pList.add_edge(key, Edge.new(key, dest, 2, rate)) if rate > 0
+      end
+    end
+    # puts @pList
+  end
+
+  def next_moves(valve, opened)
+    return @pList.points[valve].select { |m| m.cost == 1 || !opened.include?(m.destination) }
   end
 
   def process
     file = File.new(@file_name)
     @data = file.readlines.map(&:chomp)
     file.close
+  end
+end
+
+Edge = Struct.new(:name, :destination, :cost, :value)
+
+class Move
+  def initialize(time_left, gas_released, open_valves, edge)
+    @time_left = time_left
+    @gas_released = gas_released
+    @open_valves = open_valves
+    @edge_to_follow = edge
+  end
+
+  def execute
+    # puts "executing #{[@time_left, @gas_released, @open_valves]} #{@edge_to_follow}" if @open_valves[0] == "DD"
+    t = @time_left - @edge_to_follow.cost
+    @open_valves << @edge_to_follow.destination if @edge_to_follow.cost == 2
+    return [t, @gas_released + t * @edge_to_follow.value, @open_valves, @edge_to_follow.destination]
+  end
+end
+
+class PointList
+  def initialize
+    @points = {}
+  end
+
+  attr_reader :points
+
+  def add_edge(cur, edge)
+    @points[cur] = [] if @points[cur] == nil
+    @points[cur] << edge
+  end
+
+  def to_s
+    @points.each do |p, edges|
+      puts "From #{p} you can go to:"
+      edges.each do |edge|
+        puts edge
+      end
+    end
   end
 end
--- spec/day_16_spec.rb
+++ spec/day_16_spec.rb
@@ -1,27 +1,74 @@
-require 'day_16'
+require "day_16"
 
 RSpec.describe Solution16 do
   before(:each) do
-    @test_case = './lib/inputs/test_cases/day_16_1.txt'
-    @big_test = './lib/inputs/big_tests/day_16.txt'
+    @test_case = "./lib/inputs/test_cases/day_16_1.txt"
+    @big_test = "./lib/inputs/big_tests/day_16.txt"
   end
 
-  it 'solves example test case (part 1)' do
+  it "makes a move" do
+    e2 = Edge.new("AA", "DD", 2, 20)
+    m = Move.new(30, 0, [], e2)
+    expect(m.execute).to eq [28, 560, ["DD"], "DD"]
+    m = Move.new(5, 150, [], e2)
+    expect(m.execute).to eq [3, 210, ["DD"], "DD"]
+    e3 = Edge.new("AA", "II", 1, 0)
+    m = Move.new(5, 150, ["DD"], e3)
+    expect(m.execute).to eq [4, 150, ["DD"], "II"]
+    e5 = Edge.new("DD", "BB", 2, 13)
+    m = Move.new(5, 150, ["DD"], e5)
+    expect(m.execute).to eq [3, 189, ["DD", "BB"], "BB"]
+  end
+
+  it "parses a line" do
     sol = Solution16.new(@test_case)
-    expect(sol.run).to eq nil
+    sol.parse("Valve AA has flow rate=0; tunnels lead to valves DD, II, BB")
+    arr_for_not_visited = Array.new(31, -1)
+    res = { :rate => 0, :valves => ["DD", "II", "BB"], :gas_when_visited_at_t => arr_for_not_visited }
+    expect(sol.graph["AA"]).to eq res
   end
-
-  it 'solves example test case (part 2)' do
+  it "makes edges" do
     sol = Solution16.new(@test_case)
-    expect(sol.run_2).to eq nil
+    sol.make_edges
+    e1 = Edge.new("AA", "DD", 1, 0)
+    e2 = Edge.new("AA", "DD", 2, 20)
+    e3 = Edge.new("AA", "II", 1, 0)
+    e4 = Edge.new("AA", "BB", 1, 0)
+    e5 = Edge.new("AA", "BB", 2, 13)
+    expect(sol.pList.points["AA"]).to eq [e1, e2, e3, e4, e5]
+  end
+  it "finds next moves" do
+    sol = Solution16.new(@test_case)
+    sol.make_edges
+    opened = Set.new()
+    e1 = Edge.new("AA", "DD", 1, 0)
+    e2 = Edge.new("AA", "DD", 2, 20)
+    e3 = Edge.new("AA", "II", 1, 0)
+    e4 = Edge.new("AA", "BB", 1, 0)
+    e5 = Edge.new("AA", "BB", 2, 13)
+    expect(sol.next_moves("AA", opened)).to eq [e1, e2, e3, e4, e5]
+    opened.add("DD")
+    expect(sol.next_moves("AA", opened)).to eq [e1, e3, e4, e5]
+    opened.add("BB")
+    expect(sol.next_moves("AA", opened)).to eq [e1, e3, e4]
   end
 
-  it 'solves big test (part 1)' do
+  it "solves example test case (part 1)" do
+    sol = Solution16.new(@test_case)
+    expect(sol.run).to eq 1651  # site said 1651, but I keep getting 1650
+  end
+
+  it "solves example test case (part 2)" do
+    sol = Solution16.new(@test_case)
+    expect(sol.run_2).to eq 1707
+  end
+
+  it "solves big test (part 1)" do
     sol = Solution16.new(@big_test)
-    expect(sol.run).to eq nil
+    expect(sol.run).to eq 1595
   end
 
-  it 'solves big test (part 2)' do
+  it "solves big test (part 2)" do
     sol = Solution16.new(@big_test)
     expect(sol.run_2).to eq nil
   end
