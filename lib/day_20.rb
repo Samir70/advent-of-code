98bf06eee0a162fae82c611cf1b1e0d5b6e63b11
Samir70 <sbetmouni@talktalk.net>
2022-12-20 11:17:03 +0000

    solve day 20 part 1

--- lib/day_20.rb
+++ lib/day_20.rb
@@ -6,16 +6,57 @@ class Solution20
   end
 
   def run
-  
+    m = MyList.new(@data)
+    m.mix
+    return [1000, 2000, 3000].map {|el| m.get(el)}.sum
   end
 
   def run_2
-  
   end
 
   def process
     file = File.new(@file_name)
-    @data = file.readlines.map(&:chomp)
+    @data = file.readlines.map(&:chomp).map(&:to_i)
     file.close
+  end
+end
+
+class MyList
+  def initialize(arr)
+    @arr = arr.map { |el| [el, false] }
+  end
+  
+  def list
+    return @arr.map {|el| el[0]}
+  end
+
+  def mix
+    len = @arr.length
+    done = 0
+    i = 0
+    while done < len do
+      i += 1 while @arr[i][1] 
+      val = @arr[i][0]
+      destination = i + val
+      destination += len - 1 while destination <= 0 
+      destination -= len - 1 while destination >= len
+      if i < destination
+        @arr.delete_at(i)
+        @arr.insert(destination, [val, true])
+      elsif destination < i
+        @arr.delete_at(i)
+        @arr.insert(destination, [val, true])
+      else
+        @arr[i] = [val, true]
+      end
+      # puts "i, val, dest: #{[i, val, destination]} gives #{list}"
+      done += 1
+    end
+  end
+
+  def get(n)
+    idx_zero = @arr.index { |el| el[0] == 0 }
+    return nil if idx_zero == nil
+    return @arr[(n + idx_zero) % @arr.length][0]
   end
 end
--- spec/day_20_spec.rb
+++ spec/day_20_spec.rb
@@ -1,28 +1,44 @@
-require 'day_20'
+require "day_20"
 
 RSpec.describe Solution20 do
   before(:each) do
-    @test_case = './lib/inputs/test_cases/day_20_1.txt'
-    @big_test = './lib/inputs/big_tests/day_20.txt'
+    @test_case = "./lib/inputs/test_cases/day_20_1.txt"
+    @big_test = "./lib/inputs/big_tests/day_20.txt"
   end
 
-  it 'solves example test case (part 1)' do
-    sol = Solution20.new(@test_case)
-    expect(sol.run).to eq nil
+  describe MyList do
+    it "gets the nth element of a list" do
+      m = MyList.new([1, 2, -3, 4, 0, 3, -2])
+      expect(m.get(1000)).to eq 4
+      expect(m.get(2000)).to eq -3
+      expect(m.get(3000)).to eq 2
+    end
+    it "mixes array" do
+      m = MyList.new([1, 2, -3, 3, -2, 0, 4])
+      m.mix
+      expect(m.list).to eq [1, 2, -3, 4, 0, 3, -2]
+    end
   end
 
-  it 'solves example test case (part 2)' do
-    sol = Solution20.new(@test_case)
-    expect(sol.run_2).to eq nil
-  end
+  context "main tests" do
+    it "solves example test case (part 1)" do
+      sol = Solution20.new(@test_case)
+      expect(sol.run).to eq 3
+    end
 
-  it 'solves big test (part 1)' do
-    sol = Solution20.new(@big_test)
-    expect(sol.run).to eq nil
-  end
+    it "solves example test case (part 2)" do
+      sol = Solution20.new(@test_case)
+      expect(sol.run_2).to eq nil
+    end
 
-  it 'solves big test (part 2)' do
-    sol = Solution20.new(@big_test)
-    expect(sol.run_2).to eq nil
+    it "solves big test (part 1)" do
+      sol = Solution20.new(@big_test)
+      expect(sol.run).to eq 988
+    end
+
+    it "solves big test (part 2)" do
+      sol = Solution20.new(@big_test)
+      expect(sol.run_2).to eq nil
+    end
   end
 end
