b219031dfef4900f4e470e06099a68b7757b7a56
Samir70 <sbetmouni@talktalk.net>
2022-12-21 15:24:47 +0000

    solve day18 part1

--- lib/day_18.rb
+++ lib/day_18.rb
@@ -2,20 +2,77 @@ class Solution18
   def initialize(file_name)
     @file_name = file_name
     @data = []
+    @grid = nil
     process
   end
 
+  attr_reader :data, :grid
+
   def run
-  
+    faces = 0
+    @data.each do |p|
+      faces += 6
+      adj = p.neighbours
+      adj.each do |p2|
+        faces -= 1 if @grid.check(p2[0], p2[1], p2[2]) == true
+      end
+    end
+    return faces
   end
 
   def run_2
-  
   end
 
   def process
     file = File.new(@file_name)
-    @data = file.readlines.map(&:chomp)
+    @data = file.readlines
+      .map(&:chomp)
+      .map { |el| el.split(",").map(&:to_i) }
+      .map { |el| Point3D.new(el[0], el[1], el[2]) }
     file.close
+    @grid = Grid3D.new(@data)
+  end
+end
+
+class Point3D
+  def initialize(x, y, z)
+    @x = x
+    @y = y
+    @z = z
+  end
+
+  attr_reader :x, :y, :z
+
+  def neighbours
+    return [
+             [-1, 0, 0], [1, 0, 0], [0, -1, 0], [0, 1, 0], [0, 0, -1], [0, 0, 1],
+           ].map { |p| [p[0] + @x, p[1] + @y, p[2] + @z] }
+  end
+
+  def ==(other)
+    self.x == other.x && self.y == other.y && self.z == other.z
+  end
+end
+
+class Grid3D
+  def initialize(arr)
+    @length = arr.map { |el| el.x }.max
+    @width = arr.map { |el| el.y }.max
+    @height = arr.map { |el| el.z }.max
+    @grid = {}
+    arr.each do |point|
+      @grid[point.x] = {} if @grid[point.x] == nil
+      @grid[point.x][point.y] = {} if @grid[point.x][point.y] == nil
+      @grid[point.x][point.y][point.z] = true
+    end
+  end
+
+  attr_reader :grid
+
+  def check(x, y, z)
+    return "no point with x = #{x}" if @grid[x] == nil
+    return "no point with x = #{x}, y = #{y}" if @grid[x][y] == nil
+    return "no point with x = #{x}, y = #{y}, z = #{z}" if @grid[x][y][z] == nil
+    return true
   end
 end
--- spec/day_18_spec.rb
+++ spec/day_18_spec.rb
@@ -6,9 +6,33 @@ RSpec.describe Solution18 do
     @big_test = './lib/inputs/big_tests/day_18.txt'
   end
 
+  it "parses input to get an array of Point3D" do
+    sol = Solution18.new(@test_case)
+    p1 = Point3D.new(2, 2, 2)
+    p2 = Point3D.new(2, 3, 5)
+    expect(sol.data.first).to eq p1
+    expect(sol.data.last).to eq p2
+    expect(sol.data.length).to eq 13
+  end
+
+  it "can check if points are in grid" do
+    sol = Solution18.new(@test_case)
+    expect(sol.grid.check(2, 2, 2)).to eq true
+    expect(sol.grid.check(2, 3, 5)).to eq true
+    expect(sol.grid.check(4, 1, 2)).to eq "no point with x = 4"
+    expect(sol.grid.check(-1, 1, 2)).to eq "no point with x = -1"
+    expect(sol.grid.check(1, 1, 2)).to eq "no point with x = 1, y = 1"
+    expect(sol.grid.check(2, 3, 4)).to eq "no point with x = 2, y = 3, z = 4"
+  end
+
+  it "finds neighbours of a point" do
+    p1 = Point3D.new(0, 2, 2)
+    expect(p1.neighbours).to eq [[-1, 2, 2], [1, 2, 2], [0, 1, 2], [0, 3, 2], [0, 2, 1], [0, 2, 3]]
+  end
+
   it 'solves example test case (part 1)' do
     sol = Solution18.new(@test_case)
-    expect(sol.run).to eq nil
+    expect(sol.run).to eq 64
   end
 
   it 'solves example test case (part 2)' do
