82a3c261415071b68756c1dcfc239a2bdcd67a23
Samir70 <sbetmouni@talktalk.net>
2022-12-21 12:11:55 +0000

    solve day 21 -- needs loads of refactoring

--- lib/day_21.rb
+++ lib/day_21.rb
@@ -1,21 +1,128 @@
+require_relative "../utils/utils"
+
 class Solution21
   def initialize(file_name)
     @file_name = file_name
     @data = []
+    @graph = nil
     process
   end
 
+  attr_reader :data, :graph
+
   def run
-  
+    order = topological_order
+    order.each do |vert|
+      vertex = @graph.vertices[vert]
+      # puts "listening to monkey #{vertex}"
+      if vertex[:action].class == Array
+        a, op, b = vertex[:action]
+        a = @graph.vertices[a][:action]
+        b = @graph.vertices[b][:action]
+        vertex[:action] = do_op(a, op, b)
+        # puts "#{vert} knows his number #{vertex}"
+      end
+    end
+    return @graph.vertices["root"][:action]
   end
 
-  def run_2
-  
+  def run_2(humn)
+    order = topological_order
+    @graph.vertices["humn"][:action] = humn
+    order.each do |vert|
+      vertex = @graph.vertices[vert]
+      # puts "listening to monkey #{vertex}"
+      if vertex[:action].class == Array
+        a, op, b = vertex[:action]
+        a = @graph.vertices[a][:action]
+        b = @graph.vertices[b][:action]
+        vertex[:action] = do_op(a, op, b)
+        # puts "#{vert} knows his number #{vertex}"
+      end
+    end
+  end
+
+  def parse_monkey(str)
+    monkey, action = str.split(":")
+    prevs = []
+    if action.count("*/+-") == 0
+      action = action.to_i
+    else
+      action = action.split(" ")
+      prevs << action[0]
+      prevs << action[2]
+    end
+    return { :name => monkey, :action => action, :prevs => prevs, :afters => [] }
+  end
+
+  def make_graph
+    @graph = Graph.new(@data)
+  end
+
+  def topological_order
+    make_graph
+    @graph.topological_order
   end
 
   def process
     file = File.new(@file_name)
-    @data = file.readlines.map(&:chomp)
+    @data = file.readlines.map(&:chomp).map { |el| parse_monkey(el) }
     file.close
   end
+end
+
+class Graph
+  def initialize(arr)
+    @vertices = {}
+    @no_ins = []
+    @edge_count = 0
+    arr.each do |vertex|
+      name = vertex[:name]
+      if @vertices[name] == nil
+        @vertices[name] = {}
+        @vertices[name][:action] = vertex[:action]
+        @vertices[name][:prevs] = vertex[:prevs]
+        @vertices[name][:afters] = vertex[:afters]
+      else
+        @vertices[name][:action] = vertex[:action]
+        @vertices[name][:prevs] = vertex[:prevs]
+      end
+      vertex[:prevs].each do |prev|
+        @edge_count += 1
+        add_afters(prev, name)
+      end
+      @no_ins << name if vertex[:prevs].length == 0
+    end
+  end
+
+  def add_afters(v, successor)
+    if @vertices[v] == nil
+      @vertices[v] = {}
+      @vertices[v][:action] = []
+      @vertices[v][:prevs] = []
+      @vertices[v][:afters] = [successor]
+    else
+      @vertices[v][:afters] << successor
+    end
+  end
+
+  def topological_order
+    order = []
+    while @no_ins.length > 0
+      vert = @no_ins.pop
+      order << vert
+      # puts "#{vert} has empty prevs #{@vertices[vert]}"
+      @vertices[vert][:afters].each do |successor|
+        # puts "will try to remove #{vert} from #{successor}'s prevs"
+        # puts "#{@vertices[successor]}"
+        @vertices[successor][:prevs].delete(vert)
+        @no_ins << successor if @vertices[successor][:prevs].length == 0
+        @edge_count -= 1
+      end
+    end
+    puts "Edgecount is still #{@edge_count}" if @edge_count != 0
+    return order
+  end
+
+  attr_reader :no_ins, :edge_count, :vertices
 end
--- spec/day_21_spec.rb
+++ spec/day_21_spec.rb
@@ -1,28 +1,88 @@
-require 'day_21'
+require "day_21"
 
 RSpec.describe Solution21 do
   before(:each) do
-    @test_case = './lib/inputs/test_cases/day_21_1.txt'
-    @big_test = './lib/inputs/big_tests/day_21.txt'
+    @test_case = "./lib/inputs/test_cases/day_21_1.txt"
+    @big_test = "./lib/inputs/big_tests/day_21.txt"
   end
 
-  it 'solves example test case (part 1)' do
+  it "parses monkeys" do
     sol = Solution21.new(@test_case)
-    expect(sol.run).to eq nil
+    expect(sol.data[0]).to eq({ :name => "root", :action => ["pppw", "+", "sjmn"], :prevs => ["pppw", "sjmn"], :afters => [] })
+    expect(sol.data[1]).to eq({ :name => "dbpl", :action => 5, :prevs => [], :afters => [] })
   end
 
-  it 'solves example test case (part 2)' do
+  it "can list the monkeys with no dependents" do
     sol = Solution21.new(@test_case)
-    expect(sol.run_2).to eq nil
+    sol.make_graph
+    expect(sol.graph.no_ins).to eq ["dbpl", "zczc", "dvpt", "lfqf", "humn", "ljgn", "sllz", "hmdt"]
+    expect(sol.graph.edge_count).to eq 14
+    expect(sol.graph.vertices["dbpl"][:afters]).to eq ["sjmn"]
+    expect(sol.graph.vertices["hmdt"][:afters]).to eq ["drzm"]
   end
 
-  it 'solves big test (part 1)' do
-    sol = Solution21.new(@big_test)
-    expect(sol.run).to eq nil
+  it "finds a topological order for graph" do
+    sol = Solution21.new(@test_case)
+    expect(sol.topological_order).to eq ["hmdt", "sllz", "ljgn", "humn", "lfqf", "dvpt", "ptdq", "lgvd", "cczh", "pppw", "zczc", "drzm", "dbpl", "sjmn", "root"]
   end
 
-  it 'solves big test (part 2)' do
+  it "solves example test case (part 1)" do
+    sol = Solution21.new(@test_case)
+    expect(sol.run).to eq 152
+  end
+
+  it "solves example test case (part 2)" do
+    # root: pppw + sjmn
+    left = 0
+    right = 1000
+    mid = 0
+    while left < right
+      mid = left + (right - left) / 2
+      sol = Solution21.new(@test_case)
+      sol.run_2(mid)
+      pppw = sol.graph.vertices["pppw"][:action]
+      sjmn = sol.graph.vertices["sjmn"][:action]
+      # puts "humn: #{mid}, pppw: #{pppw}, sjmn: #{sjmn}"
+      if pppw == sjmn
+        left = right + 1
+      elsif pppw < sjmn
+        left = mid + 1
+      else
+        right = mid - 1
+      end
+    end
+    expect(mid).to eq 301
+  end
+
+  it "solves big test (part 1)" do
     sol = Solution21.new(@big_test)
-    expect(sol.run_2).to eq nil
+    expect(sol.run).to eq 291425799367130
+  end
+
+  it "solves big test (part 2)" do
+    left = 0
+    # right = 200860392214380
+    right = 3219579395610
+    mid = 0
+    pgtp = 0
+    vrvh = 0
+    while left < right
+      mid = left + (right - left) / 2
+      sol = Solution21.new(@big_test)
+      sol.run_2(mid)
+      pgtp = sol.graph.vertices["pgtp"][:action]
+      vrvh = sol.graph.vertices["vrvh"][:action]
+      # puts "humn: #{mid}, pgpt: #{pgtp}, vrvh: #{vrvh}"
+      if pgtp == vrvh
+        left = right + 1
+      elsif pgtp > vrvh
+        left = mid + 1
+      else
+        right = mid - 1
+      end
+    end
+    mid = nil if pgtp != vrvh
+    expect(mid).to eq 3219579395609
+    # 3219579395610 is too big
   end
 end
