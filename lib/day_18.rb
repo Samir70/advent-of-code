da7f46344f912ea6b875af678230d72dad31a889
Samir70 <sbetmouni@talktalk.net>
2022-12-21 17:50:38 +0000

    solve day 18 part 2

--- lib/day_18.rb
+++ lib/day_18.rb
@@ -21,15 +21,49 @@ class Solution18
   end
 
   def run_2
+    interior_points = Set.new()
+    for x in 0..20
+      for y in 0..20
+        for z in 0..20
+          interior_points << [x, y, z] if @grid.is_interior?(x, y, z) == true
+        end
+      end
+    end
+    # some of the points we currently list may have neighbours which are not interior
+    # ie: they are in a pocket that has a hole to the outside.
+    # spot one of these pockets by finding a point with a neighbour that is not in the list
+    # remove all points that can be reached non-diagonally from this point
+    to_remove = []
+    interior_points.each do |point|
+      x, y, z = point
+      p = Point3D.new(x, y, z)
+      adj = p.neighbours
+      to_remove << point if adj.any? { |el| !interior_points.include?(el) && @grid.check(el[0], el[1], el[2]) != true }
+    end
+    puts "to remove: #{to_remove}"
+    while to_remove.length > 0
+      x, y, z = to_remove.pop
+      p = Point3D.new(x, y, z)
+      adj = p.neighbours
+      interior_points.delete([x, y, z])
+      adj.each do |el|
+        to_remove << el if interior_points.include?(el)
+      end
+    end
+    return interior_points.to_a
   end
 
   def process
-    file = File.new(@file_name)
-    @data = file.readlines
-      .map(&:chomp)
-      .map { |el| el.split(",").map(&:to_i) }
-      .map { |el| Point3D.new(el[0], el[1], el[2]) }
-    file.close
+    if @file_name.class == String
+      file = File.new(@file_name)
+      @data = file.readlines
+        .map(&:chomp)
+        .map { |el| el.split(",").map(&:to_i) }
+        .map { |el| Point3D.new(el[0], el[1], el[2]) }
+      file.close
+    else
+      @data = @file_name.map { |el| Point3D.new(el[0], el[1], el[2]) }
+    end
     @grid = Grid3D.new(@data)
   end
 end
@@ -65,6 +99,7 @@ class Grid3D
       @grid[point.x][point.y] = {} if @grid[point.x][point.y] == nil
       @grid[point.x][point.y][point.z] = true
     end
+    # puts "made a grid of length #{@length}, width #{@width}, height #{@height}"
   end
 
   attr_reader :grid
@@ -73,6 +108,53 @@ class Grid3D
     return "no point with x = #{x}" if @grid[x] == nil
     return "no point with x = #{x}, y = #{y}" if @grid[x][y] == nil
     return "no point with x = #{x}, y = #{y}, z = #{z}" if @grid[x][y][z] == nil
+    return true
+  end
+
+  def is_interior?(x, y, z)
+    return "is part of shape" if check(x, y, z) == true
+    # puts "checking to left"
+    to_left = false
+    for i in 0...x
+      to_left = true if check(i, y, z) == true
+    end
+    return false if !to_left
+
+    # puts "checking to right"
+    to_right = false
+    for i in (x + 1)..@length
+      to_right = true if check(i, y, z) == true
+    end
+    return false if !to_right
+
+    # puts "checking to front"
+    to_front = false
+    for i in 0...y
+      to_front = true if check(x, i, z) == true
+    end
+    return false if !to_front
+
+    # puts "checking to back"
+    to_back = false
+    for i in (y + 1)..@width
+      to_back = true if check(x, i, z) == true
+    end
+    return false if !to_back
+
+    # puts "checking to down"
+    to_down = false
+    for i in 0...z
+      to_down = true if check(x, y, i) == true
+    end
+    return false if !to_down
+
+    # puts "checking to up"
+    to_up = false
+    for i in (z + 1)..@height
+      to_up = true if check(x, y, i) == true
+    end
+    return false if !to_up
+
     return true
   end
 end
--- spec/day_18_spec.rb
+++ spec/day_18_spec.rb
@@ -1,9 +1,9 @@
-require 'day_18'
+require "day_18"
 
 RSpec.describe Solution18 do
   before(:each) do
-    @test_case = './lib/inputs/test_cases/day_18_1.txt'
-    @big_test = './lib/inputs/big_tests/day_18.txt'
+    @test_case = "./lib/inputs/test_cases/day_18_1.txt"
+    @big_test = "./lib/inputs/big_tests/day_18.txt"
   end
 
   it "parses input to get an array of Point3D" do
@@ -25,28 +25,39 @@ RSpec.describe Solution18 do
     expect(sol.grid.check(2, 3, 4)).to eq "no point with x = 2, y = 3, z = 4"
   end
 
+  it "can check if a point is interior to shape of grid" do
+    sol = Solution18.new(@test_case)
+    expect(sol.grid.is_interior?(2, 2, 2)).to eq "is part of shape"
+    expect(sol.grid.is_interior?(2, 2, 5)).to eq true
+    expect(sol.grid.is_interior?(3, 3, 2)).to eq false
+  end
+
   it "finds neighbours of a point" do
     p1 = Point3D.new(0, 2, 2)
     expect(p1.neighbours).to eq [[-1, 2, 2], [1, 2, 2], [0, 1, 2], [0, 3, 2], [0, 2, 1], [0, 2, 3]]
   end
 
-  it 'solves example test case (part 1)' do
+  it "solves example test case (part 1)" do
     sol = Solution18.new(@test_case)
     expect(sol.run).to eq 64
   end
+  it "solves big test (part 1)" do
+    sol = Solution18.new(@big_test)
+    expect(sol.run).to eq 3374
+  end
 
-  it 'solves example test case (part 2)' do
+  it "solves example test case (part 2)" do
     sol = Solution18.new(@test_case)
-    expect(sol.run_2).to eq nil
+    sol2 = Solution18.new(sol.run_2)
+    expect(64 - sol2.run).to eq 58
   end
-
-  it 'solves big test (part 1)' do
+  it "solves big test (part 2)" do
     sol = Solution18.new(@big_test)
-    expect(sol.run).to eq nil
-  end
-
-  it 'solves big test (part 2)' do
-    sol = Solution18.new(@big_test)
-    expect(sol.run_2).to eq nil
+    interiors = sol.run_2
+    sol2 = Solution18.new(interiors)
+    # finds 1040 interior points
+    puts "#{interiors.length}"
+    expect(3374 - sol2.run).to eq 2010
+    # 1998 is too low
   end
 end
