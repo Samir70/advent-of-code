721826051805303bf7e564ba7ec2150c1928d853
Samir70 <sbetmouni@talktalk.net>
2022-12-23 10:15:08 +0000

    input data as FlexiGrid

--- lib/day_23.rb
+++ lib/day_23.rb
@@ -2,20 +2,108 @@ class Solution23
   def initialize(file_name)
     @file_name = file_name
     @data = []
+    @grid = nil
     process
   end
 
+  attr_reader :grid
+
   def run
-  
   end
 
   def run_2
-  
   end
 
   def process
     file = File.new(@file_name)
     @data = file.readlines.map(&:chomp)
     file.close
+    @grid = FlexiGrid.new()
+    @data.each_with_index { |line, i| @grid.add(line, i + 1) }
+    # puts @grid
+  end
+end
+
+# changed from day 22
+class Direction
+  def initialize
+    @dir = 0
+  end
+
+  attr_reader :dir
+
+  def vector(dir)
+    return @dirs[dir]
+  end
+end
+
+Dirs = {
+  "n" => [-1, 0],
+  "s" => [1, 0],
+  "e" => [0, 1],
+  "w" => [0, -1],
+}
+
+# from day 19
+def add_vector(a, b)
+  len = a.length
+  puts "#{a} and #{b} can't be added" if b.length != len
+  out = []
+  len.times do |i|
+    out << a[i] + b[i]
+  end
+  return out
+end
+
+def make_journey(loc, route)
+  vectors = route.split("").map { |d| Dirs[d] }
+  out = [].concat(loc)
+  vectors.each { |v| out = add_vector(out, v) }
+  return out
+end
+
+def nswe(r, c)
+  north = ["nw", "n", "ne"].map { |journey| make_journey([r, c], journey) }
+  south = ["sw", "s", "se"].map { |journey| make_journey([r, c], journey) }
+  west = ["sw", "w", "nw"].map { |journey| make_journey([r, c], journey) }
+  east = ["se", "e", "ne"].map { |journey| make_journey([r, c], journey) }
+  return [north, south, west, east]
+end
+
+class FlexiGrid
+  def initialize
+    @elves = Set.new()
+    @min_row = 1000000
+    @min_col = 1000000
+    @max_row = -100000
+    @max_col = -100000
+  end
+
+  attr_reader :elves, :min_row, :min_col, :max_row, :max_col
+
+  def add(str, row)
+    @min_row = row if row < @min_row
+    @max_row = row if row > @max_row
+    for c in 1..str.length
+      if str[c - 1] == "#"
+        @elves.add([row, c])
+        @min_col = c if c < @min_col
+        @max_col = c if c > @max_col
+      end
+    end
+  end
+
+  def elf_at(loc)
+    return @elves.include?(loc)
+  end
+
+  def to_s
+    for r in @min_row..@max_row do
+      row = ""
+      for c in @min_col..@max_col do
+        row += elf_at([r, c]) ? "#" : "."
+      end
+      puts row
+    end
   end
 end
--- spec/day_23_spec.rb
+++ spec/day_23_spec.rb
@@ -6,6 +6,24 @@ RSpec.describe Solution23 do
     @big_test = './lib/inputs/big_tests/day_23.txt'
   end
 
+it "finds location after a journey" do
+  expect(make_journey([1,5], "nw")).to eq [0,4]
+end
+
+  it "finds nswe of a point" do
+    expect(nswe(1, 5)).to eq [[[0,4], [0, 5], [0, 6]], [[2,4], [2,5], [2,6]], [[2,4], [1,4], [0,4]], [[2,6], [1,6], [0,6]]]
+  end
+
+  it "loads data as a flexi-grid" do
+    sol = Solution23.new(@test_case)
+    expect(sol.grid.elves.length).to eq 22
+    expect(sol.grid.min_row).to eq 1
+    expect(sol.grid.max_row).to eq 7
+    expect(sol.grid.min_col).to eq 1
+    expect(sol.grid.max_col).to eq 7
+    expect(sol.grid.elf_at([1, 5])).to eq true
+  end
+
   it 'solves example test case (part 1)' do
     sol = Solution23.new(@test_case)
     expect(sol.run).to eq nil
