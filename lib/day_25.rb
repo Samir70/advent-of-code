59f5e0599b56f74840743fdc8444817097876cb7
Samir70 <sbetmouni@talktalk.net>
2022-12-25 09:35:47 +0000

    solve day 25 part 2

--- lib/day_25.rb
+++ lib/day_25.rb
@@ -6,11 +6,12 @@ class Solution25
   end
 
   def run
-  
+    decimals = @data.map {|snafu| snafu2decimal(snafu)}
+    sum = decimals.sum
+    return pental2snafu(decimal2pental(sum))
   end
 
   def run_2
-  
   end
 
   def process
@@ -18,4 +19,46 @@ class Solution25
     @data = file.readlines.map(&:chomp)
     file.close
   end
+end
+
+def snafu_digits(s)
+  snafu = "210-="
+  digits = [2, 1, 0, -1, -2]
+  return s.split("").map { |d| snafu.index(d) }.map { |d| digits[d] } if s.class == String
+  return s.map { |d| digits.index(d) }.map { |d| snafu[d] }.join("") if s.class == Array
+end
+
+def snafu2decimal(s)
+  digits = snafu_digits(s).reverse
+  out = 0
+  col = 1
+  digits.each do |d|
+    out += d * col
+    col *= 5
+  end
+  return out
+end
+
+def pental2snafu(s)
+  num = s.class == String ? s.split("").map(&:to_i) : [].concat(s)
+  # puts "converting #{num}"
+  if num.first > 2
+    num[0] -= 5
+    num.unshift(1)
+  end
+  col = 1
+  while col < num.length do
+    if num[col] > 2
+      num[col] -= 5
+      num[col - 1] += 1
+      return pental2snafu(num)
+    end
+    col += 1
+  end
+  return snafu_digits(num)
+end
+
+def decimal2pental(num)
+  return num.to_s if num < 5
+  return decimal2pental(num / 5) + (num % 5).to_s
 end
--- spec/day_25_spec.rb
+++ spec/day_25_spec.rb
@@ -6,9 +6,29 @@ RSpec.describe Solution25 do
     @big_test = './lib/inputs/big_tests/day_25.txt'
   end
 
+  it "converts snafu to digits -2..2" do
+    expect(snafu_digits("2=")).to eq [2, -2]
+    expect(snafu_digits("210-=")).to eq [2, 1, 0, -1, -2]
+  end
+
+  it "converts snafu to decimals" do
+    expect(snafu2decimal("2=")).to eq 8
+    expect(snafu2decimal("1=-0-2")).to eq 1747
+  end
+
+  it "converts decimal to base 5" do
+    expect(decimal2pental(1747)).to eq "23442"
+  end
+
+  it "converts pental to snafu" do
+    expect(pental2snafu("400")).to eq "1-00"
+    expect(pental2snafu("300")).to eq "1=00"
+    expect(pental2snafu("23442")).to eq "1=-0-2"
+  end
+
   it 'solves example test case (part 1)' do
     sol = Solution25.new(@test_case)
-    expect(sol.run).to eq nil
+    expect(sol.run).to eq "2=-1=0" #4890 in decimal
   end
 
   it 'solves example test case (part 2)' do
