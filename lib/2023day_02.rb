1034f173eb5037e13e23368335cce8b81f8bad4c
Samir70 <sbetmouni@talktalk.net>
2023-12-02 05:53:56 +0000

    day 02 done

--- lib/2023day_02.rb
+++ lib/2023day_02.rb
@@ -5,12 +5,66 @@ class Solution02
     process
   end
 
-  def run
-  
+  def run(r, g, b)
+    sum = 0
+    @data.each do |game|
+      res = splitGame(game)
+      if res[:max][0] <= r && res[:max][1] <= g && res[:max][2] <= b
+        sum += res[:id]
+      end
+    end
+    return sum
+  end
+
+  def rgb(grab)
+    cols = grab.split(",")
+    out = {}
+    cols.each do |col|
+      out[col.split(" ")[1].to_sym] = col.split(" ")[0].to_i
+    end
+    return out
+  end
+
+  def findMax(grabList)
+    red = 0
+    green = 0
+    blue = 0
+    grabList.each do |grab|
+      if grab[:red] && grab[:red] > red 
+        red = grab[:red]
+      end
+      if grab[:green] && grab[:green] > green 
+        green = grab[:green]
+      end
+      if grab[:blue] && grab[:blue] > blue 
+        blue = grab[:blue]
+      end
+    end
+    return [red, green, blue]
+  end
+
+  def splitGame(game) 
+    split1 = game.split(":")
+    id = split1[0].split(" ")[1].to_i
+    out = {id: id}
+    grabs = split1[1].split(";").map do |el|
+      rgb(el)
+    end
+    out[:max] = findMax(grabs)
+    return out
+  end
+
+  def power(game)
+    reqd = splitGame(game)[:max]
+    return reqd[0] * reqd[1] * reqd[2]
   end
 
   def run_2
-  
+    sum = 0
+    @data.each do |game|
+      sum += power(game)
+    end
+    return sum
   end
 
   def process
--- spec/2023day_02_spec.rb
+++ spec/2023day_02_spec.rb
@@ -1,27 +1,61 @@
-require '2023day_02'
+require "2023day_02"
 
 RSpec.describe Solution02 do
   before(:each) do
-    @test_case = './lib/inputs/test_cases/2023day_02_1.txt'
-    @big_test = './lib/inputs/big_tests/2023day_02.txt'
+    @test_case = "./lib/inputs/test_cases/2023day_02_1.txt"
+    @big_test = "./lib/inputs/big_tests/2023day_02.txt"
   end
 
-  it 'solves example test case (part 1)' do
+  it "splits a grab into rbg object" do
     sol = Solution02.new(@test_case)
-    expect(sol.run).to eq nil
+    res = sol.rgb("3 blue, 4 red")
+    expect(res[:red]).to eq 4
+    expect(res[:green]).to eq nil
+    expect(res[:blue]).to eq 3
+    res = sol.rgb("1 red, 2 green, 6 blue")
+    expect(res[:red]).to eq 1
+    expect(res[:green]).to eq 2
+    expect(res[:blue]).to eq 6
   end
 
-  it 'solves example test case (part 2)' do
+  it "finds the max of each colour" do
     sol = Solution02.new(@test_case)
-    expect(sol.run_2).to eq nil
+    res = sol.findMax([{ :blue => 3, :red => 4 }, { :blue => 6, :green => 2, :red => 1 }, { :green => 2 }])
+    expect(res).to eq [4, 2, 6]
   end
 
-  it 'solves big test (part 1)' do
+  it "splits a game into id and max of rgb" do
+    sol = Solution02.new(@test_case)
+    res = sol.splitGame("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+    expect(res[:id]).to eq 1
+    expect(res[:max]).to eq [4, 2, 6]
+    res = sol.splitGame("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue")
+    expect(res[:id]).to eq 2
+    expect(res[:max]).to eq [1, 3, 4]
+  end
+
+  it "finds the power of a game" do
+    sol = Solution02.new(@test_case)
+    res = sol.power("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue")
+    expect(res).to eq 12
+  end
+
+  it "solves example test case (part 1)" do
+    sol = Solution02.new(@test_case)
+    expect(sol.run(12, 13, 14)).to eq 8
+  end
+
+  it "solves example test case (part 2)" do
+    sol = Solution02.new(@test_case)
+    expect(sol.run_2).to eq 2286
+  end
+
+  it "solves big test (part 1)" do
     sol = Solution02.new(@big_test)
-    expect(sol.run).to eq nil
+    expect(sol.run(12, 13, 14)).to eq 2204
   end
 
-  it 'solves big test (part 2)' do
+  it "solves big test (part 2)" do
     sol = Solution02.new(@big_test)
     expect(sol.run_2).to eq nil
   end
