04d1019538d8d2802ba67cafd96acb392b69103e
Samir70 <sbetmouni@talktalk.net>
2023-12-02 04:18:14 +0000

    tidy spec file

--- spec/2023day_01_spec.rb
+++ spec/2023day_01_spec.rb
@@ -1,28 +1,28 @@
-require '2023day_01'
+require "2023day_01"
 
 RSpec.describe Solution01 do
-  before(:each) do 
-    @test_case = './lib/inputs/test_cases/2023day_01_1.txt'
-    @test_case2 = './lib/inputs/test_cases/2023day_01_2.txt'
-    @big_test = './lib/inputs/big_tests/2023day_01.txt'
+  before(:each) do
+    @test_case = "./lib/inputs/test_cases/2023day_01_1.txt"
+    @test_case2 = "./lib/inputs/test_cases/2023day_01_2.txt"
+    @big_test = "./lib/inputs/big_tests/2023day_01.txt"
   end
 
-  it 'solves example test case (part 1)' do
+  it "solves example test case (part 1)" do
     sol = Solution01.new(@test_case)
     expect(sol.run).to eq 142
   end
 
-  it 'solves example test case (part 2)' do
+  it "solves example test case (part 2)" do
     sol = Solution01.new(@test_case2)
     expect(sol.run_2).to eq 281
   end
 
-  it 'solves big test (part 1)' do
+  it "solves big test (part 1)" do
     sol = Solution01.new(@big_test)
     expect(sol.run).to eq 53651
   end
 
-  it 'solves big test (part 2)' do
+  it "solves big test (part 2)" do
     sol = Solution01.new(@big_test)
     expect(sol.run_2).to eq 53894
   end
